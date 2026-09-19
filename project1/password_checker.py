c63052af396bd5b2559eeadcd2f6da96f706cd2f
Doubleo134 <oorigeneo@gmail.com>
2026-09-19 11:29:13 -0400

    Implement password strength checker
    Added password strength checker based on length and character variety.

--- project1/password_checker.py
+++ project1/password_checker.py
@@ -1,1 +1,27 @@
+import re
 
+password = input("Enter a password to check: ")
+
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if re.search(r"[A-Z]", password):
+    score += 1
+
+if re.search(r"[a-z]", password):
+    score += 1
+
+if re.search(r"[0-9]", password):
+    score += 1
+
+if re.search(r"[^A-Za-z0-9]", password):
+    score += 1
+
+if score <= 2:
+    print("Password strength: Weak")
+elif score <= 4:
+    print("Password strength: Moderate")
+else:
+    print("Password strength: Strong")
